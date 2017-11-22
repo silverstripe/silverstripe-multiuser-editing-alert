4be923347264aa1643f2531c3441d3b2e8da4f61
Adrian Stein <adrian@nimbler.com.au>
2017-11-23 09:19:37 +1100

    Update code for SS4

---
+++ js/multiUserEditing.js
@@ -0,0 +1,119 @@
+jQuery.entwine("multiUserEditing", function($) {
+    var normalImage = 'round-blue.svg';
+    var alertImage = 'round-red.svg';
+    //added third icon for use on CMS head
+    var alertImageHead = 'warning.svg';
+    var updateTimer = 0;
+
+    $('.cms-edit-form').entwine({
+        RecordID: null,
+        RecordClass: null,
+        LockURL: null,
+
+        onmatch: function() {
+            this.setCurrentPage();	//start the update timer for the first time
+        },
+
+        setUpdateTimeout: function(data){
+            var self = this;
+            clearTimeout(updateTimer);	//clear any previous timers
+
+            //set update interval approximately based on how many people are currently online
+            var updateInterval = data.update.updateIntervalMultiUser * 1000;
+            if (Object.keys(data).length <= 2) {
+                updateInterval = data.update.updateIntervalSingleUser * 1000;	//time in ms
+            }
+
+            //set the next update timer
+            updateTimer = setTimeout(function(){
+                self.setCurrentPage();
+            }, updateInterval);
+        },
+
+        setCurrentPage: function(){
+            var self = this;
+
+            //check that we are in the pages section of the site
+            var pageID = this.find('#Form_EditForm_ID').val();
+            if (typeof(pageID) !== 'undefined' && pageID !== false && pageID !== 0 && pageID !== '0') {
+
+                $.getJSON('admin/editing/set/' + pageID, function (data) {
+                    self.updateUserLabels(data, pageID);
+                    self.setUpdateTimeout(data);	//update every x seconds
+                });
+            }
+        },
+
+        updateUserLabels: function(data, currentPageID){
+            //remove all existing labels
+            var multipleEditors = false;
+            var cmsTree = $('.cms-tree');
+            //if you wish to place an icon left from CMS title use $('.cms-content-header-nav')
+            var cmsHead = $('.breadcrumbs-wrapper');
+            cmsTree.find('.user-label').remove();
+            cmsHead.find('.multi-user-message-wrap').hide();
+
+            //reorganise data into an array for each pageID;
+            var organisedData = {};
+            $.each(data, function (index, item) {
+                if (index !== 'update') {	//filter out the array key containing update intervals
+                    if (typeof(organisedData[item.pageID]) === 'undefined') {
+                        organisedData[item.pageID] = [];
+                    }
+
+                    organisedData[item.pageID].push(item);
+                }
+            });
+
+            //add new labels
+            $.each(organisedData, function (pageID, itemsArray) {
+                var treeItem = cmsTree.find('li[data-id=' + pageID + ']');
+
+                var dot = normalImage;
+                var dotHead = normalImage;
+                if (itemsArray.length > 1) {	//more than one editors on a single page, use a red dot to indicate
+                    dot = alertImage;
+                    dotHead = alertImageHead;
+                }
+
+                var users = '';	//list of all users editing the current page
+                var usersBR = '<ul class="multi-user-list">';	//list of all users separated by br instead of newline
+                $.each(itemsArray, function (index, item) {
+                    if (index !== 0) {
+                        users += "\n";	//separate users by new line
+                        //usersBR += "<br/>";
+                    }
+                    var userLine = item.fullName + ' &lt;' + item.email + '&gt;';
+                    users += userLine;
+                    usersBR += "<li>" + userLine + "</li>";
+                });
+                usersBR += "</ul>";
+
+                var dotHTML = '<div class="user-label multi-user-icon" title="' + users +
+                    '"><img height="5" width="5" ' + 'src="multiuser-editing/images/' + dot + '"></div>';
+                treeItem.find('.jstree-icon').first().after(dotHTML);
+
+                //append a highly visible message to the current page, if multiple editors are editing the same page
+                if (pageID === currentPageID && dotHead === alertImageHead) {
+
+                    if (cmsHead.find('.multi-user-message-wrap').length) {
+                        cmsHead.find('.multi-user-message-wrap').show();
+                    } else {
+                        cmsHead.prepend('<div class="multi-user-message-wrap">' +
+                            '<img height="24" width="24" ' + 'src="multiuser-editing/images/' + dotHead + '">' +
+                            '<div class="multi-user-editing-alert-message message error">' +
+                            '<span class="multi-user-editing-top-caret"></span>' +
+                            '<div class="multi-user-editing-label"></div></div></div>');
+                    }
+                    cmsHead.find('.multi-user-message-wrap .multi-user-editing-label').html('<strong>Warning</strong>: the following users are currently editing this page:<br/>' + usersBR);
+                }
+            });
+
+            return multipleEditors;
+        },
+
+        onunmatch: function(){
+            clearTimeout(updateTimer);
+        }
+    });
+});
--- src/MultiUserEditingController.php
+++ src/MultiUserEditingController.php
@@ -1,41 +1,51 @@
-<?php 
+<?php
+
+namespace MultiUserEditing;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Flushable;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Security\Security;
+use Psr\SimpleCache\CacheInterface;
 
 class MultiUserEditingController extends Controller implements Flushable
 {
-
-    public static $allowed_actions = array(
+    private static $allowed_actions = [
         'set',
         'get'
-    );
-    
+    ];
+
     private $user = null; //current user DataObject
     protected $usersEditing = null; //array of all users editing
-    protected $editingCache = null; //SS_Cache to store all concurrently editing users
+    protected $editingCache = null; //CacheInterface to store all concurrently editing users
 
-    public function init()
+    protected function init()
     {
         parent::init();
 
         //check user login status
-        $this->user = Member::currentUser();
+        $this->user = Security::getCurrentUser();
         if (!$this->user) {
             user_error("User needs to be logged in access multi-user editing data", E_USER_ERROR);
         }
 
         //get the cache data
-        $this->editingCache = SS_Cache::factory('multiuserediting');
-        $usersEditing = unserialize($this->editingCache->load('editing'));
+        $this->editingCache = Injector::inst()->get(CacheInterface::class . '.multiuserediting');
+
+
+        $usersEditing = unserialize($this->editingCache->get('editing'));
 
         //create a new simple PHP object to store user editing data
         if(!$usersEditing) {
             $usersEditing = array();
         }
-        
+
         $timeout = Config::inst()->get(get_class($this), 'userTimeoutInSeconds');
-        
+
         //remove any users that have timed out
         foreach($usersEditing as $id => $user) {
-            if (!empty($user['lastEdited']) && 
+            if (!empty($user['lastEdited']) &&
                 strtotime($user['lastEdited']) < strtotime('-'.$timeout.' seconds')) {
                 //user has timed out after above number of minutes
                 unset($usersEditing[$id]);
@@ -47,17 +57,18 @@ class MultiUserEditingController extends Controller implements Flushable
 
     public static function flush()
     {
-        SS_Cache::factory('multiuserediting')->clean(Zend_Cache::CLEANING_MODE_ALL);
+        $cache = Injector::inst()->get(CacheInterface::class . '.multiuserediting');
+        $cache->delete('multiuserediting');
     }
 
     public function set($request)
     {
         $pageID = $request->param('ID');
-        
+
         if (!intval($pageID)) {
             user_error("No page ID provided", E_USER_ERROR);
         }
-        
+
         $dataArray = array();
         $dataArray['lastEdited'] = date("Y-m-d H:i:s"); //when this was last updated
         $dataArray['abbreviatedName'] = $this->user->FirstName . ' ' . substr($this->user->Surname, 0, 1);
@@ -68,13 +79,13 @@ class MultiUserEditingController extends Controller implements Flushable
 
         //update the user editing data structure for the current user
         $this->usersEditing[$this->user->ID] = $dataArray;
-        
+
         //save to the cache
-        $this->editingCache->save(serialize($this->usersEditing), 'editing');
-        
+        $this->editingCache->set('editing', serialize($this->usersEditing));
+
         return $this->get();
     }
-    
+
     public function get()
     {
         $refresh = array(
